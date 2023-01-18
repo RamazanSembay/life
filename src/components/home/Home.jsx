445bab8b1b5556f9f239c8b395d5483de4459649
RamazanSembay <ramazansembai01@gmail.com>
2023-01-19 02:51:44 +0600

    por

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,60 +1,60 @@
-import React from 'react';
-import './home.css';
+// import React from 'react';
+// import './home.css';
 
-const Home = () => {
-  return (
-    <section className='home section' id='home'>
-      <div className='home__container container grid'>
-        <div className='home__data'>
-          <h1 className='home__title'> Было круто, <br></br> Если этот мир <br></br> Весело...</h1>
+// const Home = () => {
+//   return (
+//     <section className='home section' id='home'>
+//       <div className='home__container container grid'>
+//         <div className='home__data'>
+//           <h1 className='home__title'> Было круто, <br></br> Если этот мир <br></br> Весело...</h1>
 
-          <p className='home__description'>Тут про мой портфолио</p>
+//           <p className='home__description'>Тут про мой портфолио</p>
 
-          <form action="" className='home__search'>
-            <i className='bx bxs-map'></i>
-            <input type="search" placeholder='Search by location...' className='home__search-input' />
-            <button className='button'>Search</button>
-          </form>
+//           <form action="" className='home__search'>
+//             <i className='bx bxs-map'></i>
+//             <input type="search" placeholder='Search by location...' className='home__search-input' />
+//             <button className='button'>Search</button>
+//           </form>
 
 
-          <div className='home__value'>
-            <div>
-              <h1 className='home__value-number'>
-                9K <span>+</span>
-              </h1>
-              <span className='home__value-description'>
-              Высший сорт <br></br> Продукт
-              </span>
-            </div>
+//           <div className='home__value'>
+//             <div>
+//               <h1 className='home__value-number'>
+//                 9K <span>+</span>
+//               </h1>
+//               <span className='home__value-description'>
+//               Высший сорт <br></br> Продукт
+//               </span>
+//             </div>
 
-            <div>
-              <h1 className='home__value-number'>
-                2K <span>+</span>
-              </h1>
-              <span className='home__value-description'>
-              Счастливый <br></br> Customer
-              </span>
-            </div>
+//             <div>
+//               <h1 className='home__value-number'>
+//                 2K <span>+</span>
+//               </h1>
+//               <span className='home__value-description'>
+//               Счастливый <br></br> Customer
+//               </span>
+//             </div>
 
-            <div>
-              <h1 className='home__value-number'>
-                28K <span>+</span>
-              </h1>
-              <span className='home__value-description'>
-                Awards <br></br> Winning
-              </span>
-            </div>
-          </div>
-        </div>
+//             <div>
+//               <h1 className='home__value-number'>
+//                 28K <span>+</span>
+//               </h1>
+//               <span className='home__value-description'>
+//                 Awards <br></br> Winning
+//               </span>
+//             </div>
+//           </div>
+//         </div>
 
-        <div className='home__images'>
-          <div className='home__orbe'></div>
+//         <div className='home__images'>
+//           <div className='home__orbe'></div>
 
-          <div className='home__img'></div>
-        </div>
-      </div>
-    </section>
-  )
-}
+//           <div className='home__img'></div>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
 
-export default Home
+// export default Home
--- src/components/logos/Logos.jsx
+++ src/components/logos/Logos.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import './logos.css';
-import Img_1 from '../img/logo1.png';
-import Img_2 from '../img/logo2.png';
-import Img_3 from '../img/logo3.png';
-import Img_4 from '../img/logo4.png';
+// import React from 'react';
+// import './logos.css';
+// import Img_1 from '../img/logo1.png';
+// import Img_2 from '../img/logo2.png';
+// import Img_3 from '../img/logo3.png';
+// import Img_4 from '../img/logo4.png';
 
 
-const Logos = () => {
-  return (
-    <section className='logos section' id='logos'>
-      <div className='logos__container container grid'>
-        <div className='logos__img'>
-          <img src={Img_1} alt="" />
-        </div>
-        <div className='logos__img'>
-          <img src={Img_2} alt="" />
-        </div>
-        <div className='logos__img'>
-          <img src={Img_3} alt="" />
-        </div>
-        <div className='logos__img'>
-          <img src={Img_4} alt="" />
-        </div>
-      </div>
-    </section>
-  )
-}
+// const Logos = () => {
+//   return (
+//     <section className='logos section' id='logos'>
+//       <div className='logos__container container grid'>
+//         <div className='logos__img'>
+//           <img src={Img_1} alt="" />
+//         </div>
+//         <div className='logos__img'>
+//           <img src={Img_2} alt="" />
+//         </div>
+//         <div className='logos__img'>
+//           <img src={Img_3} alt="" />
+//         </div>
+//         <div className='logos__img'>
+//           <img src={Img_4} alt="" />
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
 
-export default Logos
+// export default Logos

9f04e1ba9c15dca168cc69ee86e03d425d6cf9df
RamazanSembay <ramazansembai01@gmail.com>
2023-01-19 02:54:52 +0600

    por

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import Home from './components/home/Home';
-import Logos from './components/logos/Logos';
 import MyHome from './components/my_home/MyHome';
 import ScrollUp from './components/scroll-up/ScrollUp';
 import Subscrible from './components/subscrible/Subscrible';
